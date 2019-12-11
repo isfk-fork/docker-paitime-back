0f172f501be988b361598d8625539768b9fb2b7a
申法宽 <shenfakuan@163.com>
2019-12-11 11:07:59 +0800

    update

--- sql/user.sql
+++ sql/user.sql
@@ -1,9 +1,11 @@
 
-CREATE DATABASE IF NOT EXISTS ypj DEFAULT CHARACTER SET = utf8mb4;
+CREATE DATABASE IF NOT EXISTS paitime DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE USER 'pt_user'@'%' IDENTIFIED BY 'RjwuGv12XSDaVsW7';
+CREATE USER 'master'@'%' IDENTIFIED BY 'RjwuGv12XSDaVsW7';
 
-GRANT ALL ON ypj.* TO 'pt_user'@'%';
+GRANT ALL ON paitime.* TO 'pt_user'@'%';
+GRANT ALL ON *.* TO 'master'@'%';
 
 flush privileges;
 
